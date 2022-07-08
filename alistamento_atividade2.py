cc606736c313d8bd18394eee019175a79ef8963d
Milton B. Assis <91283478+DevAssis@users.noreply.github.com>
2022-07-08 15:49:16 -0300

    correction in text writing

--- alistamento_atividade2.py
+++ alistamento_atividade2.py
@@ -4,6 +4,6 @@ ano_nascimento = int(input('Informe o ano de nascimento: '))
 ano = date.today().year
 idade = ano - ano_nascimento
 if idade >= 18:
-    print('Está habito a alistar')
+    print('Está hábito a alistar')
 else:
-    print('Não esta habito')
+    print('Não esta hábito')
